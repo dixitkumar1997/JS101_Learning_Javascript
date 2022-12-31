bc09e729b13d8c28d05b2b7d1b49b2f9ed3bd3f7
Dixit kumar <dxtt22@gmail.com>
2022-12-31 20:29:37 +0530

    added @nd day problems

---
+++ fw25118-Day-2-problems/problem3.js
@@ -0,0 +1,13 @@
+let a = 11;;
+
+let b = 15
+
+if(a>b){
+  console.log("a is greater");
+}
+else if(b>a){
+  console.log("b is greater");
+}
+else{
+  console.log("both equal");
+}
---
+++ fw25118-Day-2-problems/problem4.js
@@ -0,0 +1,19 @@
+let stored_username= "Dishu_hr97";
+
+let stored_password = "123456789";
+
+let username= "Dishu_hr97";
+
+let password = "123456789";
+
+if(stored_username==username){
+  if(stored_password==password){
+    console.log("user may login");
+  }
+  else{
+    console.log("password is not correct");
+  }
+}
+  else{
+    console.log("username is not correct");
+  }
